14ecdf82ad8f89ad24e493127f6a0772884002fe
mkhajiit <mkhajiit15@gmail.com>
2025-12-16 23:36:57 +0900

    feat: 문자열 뒤집기 추가

--- C#/Practice/First/ArraySolutions.cs
+++ C#/Practice/First/ArraySolutions.cs
@@ -5,6 +5,7 @@ class ArraySolutions
     int[] test = {10,20,40,60,100};
     int[] reversedTest = ReverseArray(test);
     Console.WriteLine($"[{string.Join(",",reversedTest)}]");
+    Console.WriteLine($"뒤집힌 문자열: {ReverseString("start")}");
   }
 
   private int[] ReverseArray(int[] source)
@@ -17,5 +18,13 @@ class ArraySolutions
     }
 
     return result;
+  }
+
+  private string ReverseString(string source)
+  {
+    var array = source.Reverse().ToArray(); //배열이 아닌 열거 시퀀스 상태(Reverse) => 배열상태로 변환한다(ToArray)
+    string reversed = new string(array); // 배열 => 문자열
+
+    return reversed;
   }
 }
