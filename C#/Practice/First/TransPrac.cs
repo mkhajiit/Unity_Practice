44c7e2835b774cb6f88b6ee0366f21f122072f0c
mkhajiit <mkhajiit15@gmail.com>
2025-12-21 23:32:17 +0900

    feat: Concat, Enumerable 활용

--- C#/Practice/First/TransPrac.cs
+++ C#/Practice/First/TransPrac.cs
@@ -5,8 +5,16 @@ class TransPrac
     int a = 12;
     int b = 3;
 
+    string asbing = "aisa";
+    string bs = "bisb";
+
     string result = a.ToString() + b.ToString();
 
+    string secondResult = asbing.Substring(1,2) + bs.Substring(0,3); 
+
+    string answer = string.Concat(Enumerable.Repeat(secondResult,5));
+
     Console.WriteLine(int.Parse(result));
+    Console.WriteLine(answer);
   }
 }
